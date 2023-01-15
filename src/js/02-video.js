e841e2dca2eccb676dbab4ee3c30be62d1387f15
cldblz <cloudyshogun@gmail.com>
2023-01-15 12:42:12 +0600

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(e) {
+    localStorage.setItem("videoplayer-current-time", e.seconds);
+}
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
